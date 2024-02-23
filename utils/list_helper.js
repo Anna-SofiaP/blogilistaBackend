312205187737a1a2baa6e7769ca25b003d8b949c
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2024-02-23 14:40:04 +0200

    osa 4 - tehtävää 4.2 aloitettu

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,7 +1,13 @@
 const dummy = (blogs) => {
     return 1
 }
+
+const totalLikes = (blogs) => {
+    let initValue = 0
+    const sum = blogs.reduce((acc, curr) => acc + curr, initValue)
+}
   
 module.exports = {
-    dummy
+    dummy,
+    totalLikes,
 }
