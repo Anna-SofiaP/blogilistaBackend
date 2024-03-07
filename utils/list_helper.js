c064fdd757bd46714fdf8c856c2fdacedfedec58
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2024-03-07 14:04:54 +0200

    osa4 - tehtävä 4.5 tehty

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -7,8 +7,25 @@ const totalLikes = (blogs) => {
     const sum = blogs.reduce((acc, currentVal) => acc + currentVal.likes, initValue)
     return sum
 }
+
+const favoriteBlog = (blogs) => {
+    if (blogs.length === 0) return 0
+    let favorite = blogs[0]
+    for (let i = 0; i < blogs.length; i++) {
+        if (blogs[i].likes > favorite.likes) {
+            favorite = blogs[i]
+        }
+    }
+    const newFavorite = {
+        title: favorite.title,
+        author: favorite.author,
+        likes: favorite.likes
+    }
+    return newFavorite
+}
   
 module.exports = {
     dummy,
     totalLikes,
+    favoriteBlog,
 }
