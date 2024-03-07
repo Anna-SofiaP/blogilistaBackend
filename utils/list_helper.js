6a58bcd99b2d1b08abe4366d2b80669d05f2df97
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2024-03-07 14:48:18 +0200

    aloitettu 4.6

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -23,9 +23,45 @@ const favoriteBlog = (blogs) => {
     }
     return newFavorite
 }
+
+const mostBlogs = (blogs) => {
+    if (blogs.length === 0) return 0
+    
+    let authors = [blogs[0].author]
+    let blogAmount = [1]
+
+    for (let i = 1; i < blogs.length; i++) {
+        let current = blogs[i].author
+        if (authors.includes(current)) {
+            let index = authors.indexOf(current)
+            blogAmount[index]++
+        } else {
+            authors.concat(current)
+            blogAmount.concat(1)
+        }
+    }
+
+    let greatest = blogAmount[0]
+    let indexOfGreatest = 0
+
+    for (let i = 1; i < blogAmount.length; i++) {
+        if (blogAmount[i] > greatest) {
+            greatest = blogAmount[i]
+            indexOfGreatest = i
+        }
+    }
+
+    const mostBlogs = {
+        author: authors[indexOfGreatest],
+        blogs: greatest
+    }
+
+    return mostBlogs
+}
   
 module.exports = {
     dummy,
     totalLikes,
     favoriteBlog,
+    mostBlogs,
 }
