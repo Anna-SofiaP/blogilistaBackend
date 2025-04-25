fb7735666ab8adb260f0958e200aeeed1e3cf22f
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2025-04-25 19:16:23 +0300

    osa4 - tehtävä 4.17 tehty

--- controllers/blogsroutes.js
+++ controllers/blogsroutes.js
@@ -1,10 +1,11 @@
 const blogsRouter = require('express').Router()
 const { request } = require('../app')
 const Blog = require('../models/blog')
+const User = require('../models/user')
 const logger = require('../utils/logger')
 
 blogsRouter.get('/', async (request, response) => {
-    const blogs = await Blog.find({})
+    const blogs = await Blog.find({}).populate('user', { username: 1, id: 1 })
     logger.info('All blogs: ', blogs)
     response.json(blogs)
 })
@@ -12,16 +13,22 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
     const body = request.body
+
+    const user = await User.findById(body.userID)
     
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
-        likes: body.likes
+        likes: body.likes,
+        user: user._id
     })
     logger.info('Adding new post... ', blog)
 
     const result = await blog.save()
+    user.blogs = user.blogs.concat(blog._id)
+    await user.save()
+
     logger.info('New post added! ', result)
     response.status(201).json(result)
 })
