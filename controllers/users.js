495cd0cf12422c6ce5693b61873acbb6e8cbe06c
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2025-04-25 17:28:34 +0300

    osa4 - tehtävä 4.15 tehty

---
+++ controllers/users.js
@@ -0,0 +1,30 @@
+const bcryptjs = require('bcryptjs')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+const logger = require('../utils/logger')
+
+usersRouter.post('/', async (request, response) => {
+  const { username, name, password } = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcryptjs.hash(password, saltRounds)
+
+  const user = new User({
+    username,
+    name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.status(201).json(savedUser)
+})
+
+
+usersRouter.get('/', async (request, response) => {
+    const users = await User.find({})
+    logger.info('All users: ', users)
+    response.json(users)
+})
+
+module.exports = usersRouter
