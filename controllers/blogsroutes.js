dc222607808a0f4ab9e6d4d9eb0667f1b15d094f
ansupaav <anna-sofia.s.paavonen@student.jyu.fi>
2025-04-29 14:00:23 +0300

    osa4 - tehtävä 4.19 tehty

--- controllers/blogsroutes.js
+++ controllers/blogsroutes.js
@@ -3,6 +3,17 @@ const { request } = require('../app')
 const Blog = require('../models/blog')
 const User = require('../models/user')
 const logger = require('../utils/logger')
+const jwt = require('jsonwebtoken')
+
+const getToken = (request) => {
+    // Get value of the authorization header from request body
+    const auth = request.get('authorization')
+    if (auth && auth.startsWith('Bearer ')) {
+        // return only the token
+        return auth.replace('Bearer ', '')
+    }
+    return null
+}
 
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({}).populate('user', { username: 1, id: 1 })
@@ -11,10 +22,22 @@ blogsRouter.get('/', async (request, response) => {
 })
   
 
-blogsRouter.post('/', async (request, response) => {
+blogsRouter.post('/', async (request, response, next) => {
     const body = request.body
 
-    const user = await User.findById(body.userID)
+    try {
+        // Decode the token that was sent with the request
+        const decodedToken = jwt.verify(getToken(request), process.env.SECRET)
+        if (!decodedToken.id) {
+            return response.status(401).json({ error: 'invalid token' })
+        }
+    }
+    catch(error) {
+        next(error)
+    }
+    
+
+    const user = await User.findById(decodedToken.id)
     
     const blog = new Blog({
         title: body.title,
